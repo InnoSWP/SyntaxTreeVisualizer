030e450fa25913c43ecb51cb95c2f0eb82fd704e
Fil126 <g.fil@innopolis.university>
2022-06-10 20:52:33 +0800

    Added default page layout and codemirror library

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,4 +1,15 @@
 import React, {Component} from 'react';
+import {EditorState} from "@codemirror/state"
+import {EditorView} from "codemirror"
+import {defaultKeymap} from "@codemirror/commands"
+import {
+    drawSelection,
+    dropCursor,
+    highlightActiveLine, highlightSpecialChars,
+    keymap,
+    lineNumbers,
+    placeholder
+} from "@codemirror/view";
 
 export default class Home extends Component {
     constructor(props) {
@@ -10,13 +21,42 @@ export default class Home extends Component {
 
     async componentDidMount() {
         this.setState({});
+        let startState = EditorState.create({
+            extensions: [
+                placeholder("// put your code here"),
+                lineNumbers(),
+                dropCursor(),
+                drawSelection(),
+                highlightSpecialChars(),
+                highlightActiveLine(),
+                keymap.of(defaultKeymap),
+                EditorView.theme({
+                    "&.cm-focused": {
+                        outline: "0px !important"
+                    }
+                })
+            ]
+        })
+
+        let view = new EditorView({
+            state: startState,
+            parent: document.getElementById("editor"),
+            mode: "javascript"
+        })
     }
 
 
     render() {
         return (
             <div className="app">
-                <h1>Base</h1>
+                <div className="row m-0" style={{padding: '30px', boxSizing: 'border-box'}}>
+                    <div className="col" style={{minHeight: '90vh', border: '1px solid'}}>
+                        <div id="editor"></div>
+                    </div>
+                    <div className="col" style={{minHeight: '90vh', border: '1px solid'}}>
+                        Column
+                    </div>
+                </div>
             </div>
         );
     }
