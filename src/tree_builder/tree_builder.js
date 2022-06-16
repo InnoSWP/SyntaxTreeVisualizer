7b711ab4e81a02db3a744d6b2b687a021a2def4f
Fil126 <g.fil@innopolis.university>
2022-06-16 15:52:06 +0800

    "BinaryExpression" and "Literal" objects building added

--- src/tree_builder/tree_builder.js
+++ src/tree_builder/tree_builder.js
@@ -7,6 +7,15 @@ export function get_tree(obj, nodes=[], edges=[])
         case "Program":
             return build_program(obj, nodes, edges);
 
+        case "ExpressionStatement":
+            return get_tree(obj.expression, nodes, edges);
+
+        case "BinaryExpression":
+            return build_binary_expression(obj, nodes, edges);
+
+        case "Literal":
+            return build_literal(obj, nodes, edges);
+
         default:
             return build_default(obj, nodes, edges);
     }
@@ -50,6 +59,56 @@ function build_program(obj, nodes=[], edges=[])
         nodes = sub_result.nodes;
         edges = sub_result.edges;
     }
+
+    return {
+        nodes: nodes,
+        edges: edges
+    };
+}
+
+function build_binary_expression(obj, nodes=[], edges=[])
+{
+    let root = count;
+
+    nodes.push({
+        id: count+"",
+        text: obj.operator
+    });
+    count++;
+
+    edges.push({
+        id: root+"->"+count,
+        from: root+"",
+        to: count+""
+    });
+
+    let sub_result = get_tree(obj.left, nodes, edges);
+    nodes = sub_result.nodes;
+    edges = sub_result.edges;
+
+    edges.push({
+        id: root+"->"+count,
+        from: root+"",
+        to: count+""
+    });
+
+    sub_result = get_tree(obj.right, nodes, edges);
+    nodes = sub_result.nodes;
+    edges = sub_result.edges;
+
+    return {
+        nodes: nodes,
+        edges: edges
+    };
+}
+
+function build_literal(obj, nodes=[], edges=[])
+{
+    nodes.push({
+        id: count+"",
+        text: (typeof obj.value == "string" ? '"' : "") + obj.value+"" + (typeof obj.value == "string" ? '"' : "")
+    });
+    count++;
 
     return {
         nodes: nodes,
