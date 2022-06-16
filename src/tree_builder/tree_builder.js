62f5803940e1de1761a448a08e06f7ad0e30e932
Fil126 <g.fil@innopolis.university>
2022-06-16 15:50:21 +0800

    "program" object building added

--- src/tree_builder/tree_builder.js
+++ src/tree_builder/tree_builder.js
@@ -1,23 +1,55 @@
+let count = 0;
+
 export function get_tree(obj, nodes=[], edges=[])
 {
-    nodes = [
-        {
-            id: '1',
-            text: '1'
-        },
-        {
-            id: '2',
-            text: '2'
-        }
-    ];
+    switch (obj.type)
+    {
+        case "Program":
+            return build_program(obj, nodes, edges);
 
-    edges = [
-        {
-            id: '1-2',
-            from: '1',
-            to: '2'
-        }
-    ];
+        default:
+            return build_default(obj, nodes, edges);
+    }
+}
+
+function build_default(obj, nodes=[], edges=[])
+{
+    nodes.push({
+        id: count+"",
+        text: obj.type
+    });
+    count++;
+
+    return {
+        nodes: nodes,
+        edges: edges
+    };
+}
+
+function build_program(obj, nodes=[], edges=[])
+{
+    let root = count;
+
+    nodes.push({
+        id: count+"",
+        text: "Program"
+    });
+    count++;
+
+    for (let i = 0; i < obj.body.length; i++)
+    {
+        let child = obj.body[i];
+
+        edges.push({
+            id: root+"->"+count,
+            from: root+"",
+            to: count+""
+        });
+
+        let sub_result = get_tree(child, nodes, edges);
+        nodes = sub_result.nodes;
+        edges = sub_result.edges;
+    }
 
     return {
         nodes: nodes,
