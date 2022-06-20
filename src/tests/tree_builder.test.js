b2f7ca561338212bca2c2553057526ba5cd22001
Fil126 <g.fil@innopolis.university>
2022-06-20 19:11:19 +0800

    Simple test added

---
+++ src/tests/tree_builder.test.js
@@ -0,0 +1,42 @@
+import {get_tree} from "../tree_builder/tree_builder";
+import {JavaScriptParser} from "../parser/JavaScriptParser";
+
+test("Simple test 1+1", () => {
+    expect(get_tree(new JavaScriptParser().parse("1+1"), [], [])).toStrictEqual({
+        "edges": [
+            {
+                "from": "0",
+                "id": "0->1",
+                "to": "1",
+            },
+            {
+                "from": "1",
+                "id": "1->2",
+                "to": "2",
+            },
+            {
+                "from": "1",
+                "id": "1->3",
+                "to": "3",
+            },
+        ],
+        "nodes": [
+            {
+                "id": "0",
+                "text": "Program",
+            },
+            {
+                "id": "1",
+                "text": "+",
+            },
+            {
+                "id": "2",
+                "text": "1",
+            },
+            {
+                "id": "3",
+                "text": "1",
+            },
+        ]
+    });
+});
