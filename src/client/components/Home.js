1ec8a8ed9b8e0b443ca013269b88ab98e00d2881
Markov Andrei <me@markovav.ru>
2022-06-28 20:56:21 +0300

    Fixed SonarCloud issues

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -162,8 +162,8 @@ let getUrlParameter = function getUrlParameter(sParam) {
     for (i = 0; i < sURLVariables.length; i++) {
         sParameterName = sURLVariables[i].split('=');
         if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            return sParameterName[1] === undefined ? undefined : decodeURIComponent(sParameterName[1]);
         }
     }
-    return false;
+    return undefined;
 };
