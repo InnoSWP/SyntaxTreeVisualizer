11842d398e890d41c6c87cd5139f7367457aa9ba
Fil-126 <grigorii_fil@inbox.ru>
2022-06-30 03:59:12 +0800

    Revert "CodeSmells fixed"
    This reverts commit badd34ee8e5adf6e6673f8801017b83e422364e6.

--- src/tree_builder/tree_builder.js
+++ src/tree_builder/tree_builder.js
@@ -302,7 +302,7 @@ function build_function_declaration(obj, nodes, edges, depth)
             text: "arg " + (i+1)
         });
 
-        sub_result = get_tree(obj.params[i], nodes, edges, depth);
+        let sub_result = get_tree(obj.params[i], nodes, edges, depth);
         nodes = sub_result.nodes;
         edges = sub_result.edges;
     }
@@ -428,7 +428,7 @@ function build_for_statement(obj, nodes, edges, depth)
             text: "init"
         });
 
-        sub_result = get_tree(obj.init, nodes, edges, depth);
+        let sub_result = get_tree(obj.init, nodes, edges, depth);
         nodes = sub_result.nodes;
         edges = sub_result.edges;
     }
@@ -442,7 +442,7 @@ function build_for_statement(obj, nodes, edges, depth)
             text: "condition"
         });
 
-        sub_result = get_tree(obj.test, nodes, edges, depth);
+        let sub_result = get_tree(obj.test, nodes, edges, depth);
         nodes = sub_result.nodes;
         edges = sub_result.edges;
     }
@@ -456,7 +456,7 @@ function build_for_statement(obj, nodes, edges, depth)
             text: "update"
         });
 
-        sub_result = get_tree(obj.update, nodes, edges, depth);
+        let sub_result = get_tree(obj.update, nodes, edges, depth);
         nodes = sub_result.nodes;
         edges = sub_result.edges;
     }
@@ -700,7 +700,7 @@ function build_method_definition(obj, nodes, edges, depth)
             text: "arg " + (i+1)
         });
 
-        sub_result = get_tree(obj.value.params[i], nodes, edges, depth);
+        let sub_result = get_tree(obj.value.params[i], nodes, edges, depth);
         nodes = sub_result.nodes;
         edges = sub_result.edges;
     }
@@ -755,7 +755,7 @@ function build_class_declaration(obj, nodes, edges, depth)
             text: "super"
         });
 
-        sub_result = get_tree(obj.superClass, nodes, edges, depth);
+        let sub_result = get_tree(obj.superClass, nodes, edges, depth);
         nodes = sub_result.nodes;
         edges = sub_result.edges;
     }
